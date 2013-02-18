186c87779bb89f6f74cb991b09b0d438d97ed246
Matthew Kenny <matthewkenny@gmail.com>
2013-02-17 17:08:31 -0800

    Added some comments.

--- assignments/week06/athome/djangor/djangor/urls.py
+++ assignments/week06/athome/djangor/djangor/urls.py
@@ -18,10 +18,8 @@ urlpatterns = patterns('',
     url(r'^add/', EntryCreate.as_view(), name='entry_create'),
     # Superseded by specific entries for register and default auth_urls (see below).
     # url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^register/$',
-                           register,
-                           {'backend': 'registration.backends.simple.SimpleBackend', 'success_url':'/'},
-                           name='registration_register'),
+    url(r'^register/$',register, {'backend': 'registration.backends.simple.SimpleBackend',
+                                  'success_url':'/'}, name='registration_register'),
     # For login logout. See /Users/matt/Projects/scipy/lib/python2.7/site-packages/registration/backends/simple/urls.py
     url(r'', include('registration.auth_urls')),
     )
