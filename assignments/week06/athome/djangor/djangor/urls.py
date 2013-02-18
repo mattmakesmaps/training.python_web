7fc7c6ca59af11992ad86f387a3cef3c498a8fd8
Matthew Kenny <matthewkenny@gmail.com>
2013-02-18 14:49:19 -0800

    Added some URL Reversing for different views.

--- assignments/week06/athome/djangor/djangor/urls.py
+++ assignments/week06/athome/djangor/djangor/urls.py
@@ -10,7 +10,7 @@ from registration.views import register
 
 urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
@@ -18,8 +18,10 @@ urlpatterns = patterns('',
     url(r'^add/', EntryCreate.as_view(), name='entry_create'),
     # Superseded by specific entries for register and default auth_urls (see below).
     # url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^register/$',register, {'backend': 'registration.backends.simple.SimpleBackend',
-                                  'success_url':'/'}, name='registration_register'),
+    url(r'^register/$',
+                           register,
+                           {'backend': 'registration.backends.simple.SimpleBackend', 'success_url':'/'},
+                           name='registration_register'),
     # For login logout. See /Users/matt/Projects/scipy/lib/python2.7/site-packages/registration/backends/simple/urls.py
     url(r'', include('registration.auth_urls')),
     )
